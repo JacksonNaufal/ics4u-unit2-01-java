c3d79b623836fd381ab5cbead05e6d354eece1fa
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-10-06 18:16:01 -0400

    Update JacksonStacks.java

--- JacksonStacks.java
+++ JacksonStacks.java
@@ -1,16 +1,43 @@
+/*
+ * This is a JacksonStacks Class
+ *
+ * @author Jackson Naufal
+ * @version 1.0
+ * @since 2022-10-06
+ */
 
 import java.util.ArrayList;
 
+/**
+ * This is a class for my
+ * program.
+ * JacksonStacks Class
+ */
+
 public class JacksonStacks {
 
-      private ArrayList<Integer> stackNum = new ArrayList<Integer>();
+    /**
+     * This is my array which is only editable
+     * when you are in this class.
+     */
+    private ArrayList<Integer> stackNum = new ArrayList<Integer>();
 
-        public void push(int userInt) {
-                stackNum.add(userInt);
-        }
-        
-        public void finishedStack() {
-                System.out.println(stackNum);       
-      }
-
+    /**
+     * This is a function for the pushed item.
+     *
+     * @param userInt the number thats added to the array.
+     *
+     */
+    public void push(int userInt) {
+        stackNum.add(userInt);
     }
+
+    /**
+     * This is the function that gets the stack
+     * ready to be printed.
+     */
+    public void finishedStack() {
+        System.out.println(stackNum);
+    }
+
+}
